ea70335e26507ff71477420ab3570a0699dae2de
Ankitwart021 <wartankit006@gmail.com>
2025-02-15 22:10:11 +0530

    Time: 2 ms (97.93%), Space: 43.5 MB (63.99%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        char[] s1 = s.toCharArray();
-        char[] s2 = t.toCharArray();
-        Arrays.sort(s1);
-        Arrays.sort(s2);
-        return Arrays.equals(s1, s2);
+        int[] count = new int[26];
+        for(char x : s.toCharArray()){
+            count[x - 'a']++;
+        }
+        for(char x : t.toCharArray()){
+            count[x - 'a']--;
+        }
+        for(int val : count){
+            if(val != 0) return false;
+        }
+        return true;
     }
 }
