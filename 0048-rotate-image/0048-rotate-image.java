3148a10669f484ba68001b5eda2a3ea2d2a09343
Ankitwart021 <wartankit006@gmail.com>
2025-02-21 17:55:25 +0530

    Time: 0 ms (100%), Space: 42.3 MB (57.61%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -9,8 +9,7 @@ class Solution {
         }
         return matrix;
     }
-    public void rotate(int[][] matrix) {
-        matrix = transpose(matrix);
+    public void rotate90(int[][] matrix){
         for(int i = 0; i < matrix.length; i++){
             for(int j = 0; j < matrix[0].length / 2; j++){
                 int temp = matrix[i][j];
@@ -18,5 +17,9 @@ class Solution {
                 matrix[i][matrix[0].length - 1 - j] = temp;
             }
         }
+    }
+    public void rotate(int[][] matrix) {
+        matrix = transpose(matrix);
+        rotate90(matrix);
     }
 }
